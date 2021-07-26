6fd302e5ec8d7f070c56411261f615cf0d427f06
Vijayant Katyal <1795103+vijayantkatyal@users.noreply.github.com>
2021-07-26 23:08:43 +0530

    [Added] site settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 use IsotopeKit\AuthAPI\Models\User;
 use IsotopeKit\AuthAPI\Models\User_Role;
 use IsotopeKit\AuthAPI\Models\Levels;
+use IsotopeKit\AuthAPI\Models\Site;
 
 class DatabaseSeeder extends Seeder
 {
@@ -31,6 +32,13 @@ class DatabaseSeeder extends Seeder
 		User_Role::insert([
 			'user_id'	=>	'1',
 			'levels'	=>	'["0"]'
+		]);
+
+		Site::insert([
+			'language'	=>	'en',
+			'theme'		=>	'blue',
+			'name'		=>	'IsotopeKit',
+			'agency_id'	=>	'1'
 		]);
 
 		Levels::insert([
---
+++ src/Models/Site.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace IsotopeKit\AuthAPI\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Site extends Model
+{
+    protected $table = 'site_settings';
+
+    protected $fillable = [
+        'language','theme', 'name', 'logo'
+    ];
+
+    public static function settings()
+    {
+        $url = $_SERVER['HTTP_HOST'];
+        
+        if(strpos($url, env('APP_URL_DOMAIN')) != false)
+        {
+            $parsedUrl = parse_url($url);
+            // $host = explode('.', $parsedUrl['host']);
+            // for server
+            $host = explode('.', $parsedUrl['path']);
+            $subdomain = $host[0];
+            
+            if($subdomain != "dashboard")
+            {
+                $site = Site::where('unique_name', $subdomain)->first();
+                if($site)
+                {
+                    return $site;
+                }
+            }
+        }
+        else
+        {
+            // return Site::where('id', 2)->first();
+
+            $url_1 = "http://".$url;
+            $url_2 = "https://".$url;
+
+            $site = Site::where('external_url', $url_1)->orWhere('external_url', $url_2)->first();
+            if($site)
+            {
+                return $site;
+            }
+        }
+        
+        // app
+        return Site::first();
+
+        // agency
+        // return Site::where('id', 2)->first();
+    }
+}
